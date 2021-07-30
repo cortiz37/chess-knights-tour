c2e230d97909017d59d667ba251b1bb83dbc3eb3
cortiz37 <cortiz37@gmail.com>
2021-07-30 10:22:27 -0500

    knight tour ui.

--- front-end/src/app/routes/views/Chess.js
+++ front-end/src/app/routes/views/Chess.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import {Divider, Radio} from 'antd';
+import {Alert, Button, Card, List, notification, Tabs} from 'antd';
 import Chessboard from "chessboardjsx";
+import {CloseCircleOutlined, RightCircleOutlined} from "@ant-design/icons";
+import {knightTour} from "../../../service/ChessService";
+
+const {TabPane} = Tabs;
 
 class Chess extends Component {
 
@@ -10,15 +14,20 @@ class Chess extends Component {
             width: 0,
             height: 0,
             mode: 'tour',
-            position: {}
+            blocked: false,
+            allowMoves: true,
+            position: {},
+            size: 400
         };
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
 
-    updateMode(e) {
+    updateMode(key) {
         this.setState({
-            mode: e.target.value,
-            position: {}
+            mode: key,
+            position: {},
+            allowMoves: true,
+            blocked: false
         })
     }
 
@@ -46,6 +55,55 @@ class Chess extends Component {
         }
     }
 
+    start() {
+        if (this.state.mode === 'tour') {
+            this.setState({
+                blocked: true,
+                allowMoves: false
+            });
+            knightTour(Object.keys(this.state.position)[0])
+                .then(response => {
+                    response.json().then(json => {
+                        const moves = json.steps.map(step => {
+                            const data = step.split(',');
+                            return String.fromCharCode(parseInt(data[0]) + 97) + (parseInt(data[1]) + 1);
+                        });
+                        this.setState({
+                            moves: moves,
+                            currentMove: moves[0]
+                        });
+                    })
+                }).catch(error => {
+                    notification.open({
+                        message: 'Error',
+                        description: 'Something went wrong!',
+                        icon: <CloseCircleOutlined/>,
+                    });
+                }
+            );
+        }
+    }
+
+    next() {
+        const moves = this.state.moves;
+        const nextIndex = moves.indexOf(this.state.currentMove) + 1;
+        if (nextIndex < moves.length) {
+            const nextMove = moves[nextIndex];
+            const currentMove = this.state.currentMove;
+            const position = this.state.position;
+
+            position[currentMove] = 'bN';
+            position[nextMove] = 'wN';
+
+            const size = this.state.size > 401 ? 400 : this.state.size + 1;
+            this.setState({
+                position: position,
+                currentMove: nextMove,
+                size: size
+            });
+        }
+    }
+
     componentDidMount() {
         this.updateWindowDimensions();
         window.addEventListener('resize', this.updateWindowDimensions);
@@ -60,28 +118,73 @@ class Chess extends Component {
     }
 
     render() {
-        return <div className="miau">
+        return <div>
             <Chessboard
                 onSquareClick={target => this.onClick(target)}
                 onDrop={e => this.onDrop(e)}
                 boardStyle={{float: 'left', marginRight: '20px'}}
+                draggable={this.state.allowMoves}
                 calcWidth={() => {
-                    const defaultSize = 400;
+                    const defaultSize = this.state.size;
                     const min = this.state.width < this.state.height ? this.state.width : this.state.height;
                     return min < defaultSize ? min : defaultSize;
                 }} position={this.state.position}/>
-            <div>
-                <Radio.Group defaultValue={this.state.mode} buttonStyle="solid"
-                             onChange={value => this.updateMode(value)}>
-                    <Radio.Button value="tour">Knight's tour</Radio.Button>
-                    <Radio.Button value="capture">Capture piece</Radio.Button>
-                </Radio.Group>
-                {
-                    this.state.mode === 'tour' && this.isEmptyPosition() ?
-                        <p>Click on the board to place the knight</p>
-                        :
-                        <p>START...</p>
-                }
+            <div style={{float: 'right'}}>
+                <Card>
+                    <div>
+                        <Tabs defaultActiveKey="tour" onChange={value => this.updateMode(value)}>
+                            <TabPane tab="Knight's tour" key="tour">
+                                <Card>
+                                    {
+                                        this.isEmptyPosition() ?
+                                            <Alert showIcon message="Click on the board to place the knight"
+                                                   type="warning"/>
+                                            :
+                                            <div>
+                                                {
+                                                    this.state.blocked ?
+                                                        <div>
+                                                            <div>
+                                                                <Button onClick={() => this.next()} type="primary"
+                                                                        shape="round" icon={<RightCircleOutlined/>}>
+                                                                    Next
+                                                                </Button>
+                                                            </div>
+                                                            <div className="scrollable-container top-margin">
+                                                                <List
+                                                                    size="small"
+                                                                    header={<div>Moves</div>}
+                                                                    bordered
+                                                                    dataSource={this.state.moves}
+                                                                    renderItem={
+                                                                        item =>
+                                                                            <List.Item>{item === this.state.currentMove ? item + " (current)" : item}</List.Item>
+                                                                    }
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        :
+                                                        <div>
+                                                            <Alert showIcon
+                                                                   message="The knight can be moved to change the starting position"
+                                                                   type="info"/>
+                                                            <div className="top-margin"></div>
+                                                            <Button onClick={() => this.start()} type="primary"
+                                                                    shape="round" icon={<RightCircleOutlined/>}>
+                                                                Start
+                                                            </Button>
+                                                        </div>
+                                                }
+                                            </div>
+                                    }
+                                </Card>
+                            </TabPane>
+                            <TabPane tab="Capture piece" key="capture">
+                                Content of Tab Pane 2
+                            </TabPane>
+                        </Tabs>
+                    </div>
+                </Card>
             </div>
         </div>
     }
--- front-end/src/service/ChessService.js
+++ front-end/src/service/ChessService.js
@@ -9,7 +9,12 @@ export function canCapture(pieceType, captureRequest) {
     });
 }
 
-export function knightTour(position, tourRequest) {
+export function knightTour(position) {
+    const tourRequest = {
+        sourceColumn: position.charAt(0).charCodeAt(0) - 97,
+        sourceRow: parseInt(position.charAt(1)) - 1,
+        boardSize: 8
+    };
     return request({
         url: API_URL + '/tour',
         method: 'POST',
